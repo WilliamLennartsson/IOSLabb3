a19acabd404c2687014502071499b1f0d6222a87
WilliamLennartsson <31884043+WilliamLennartsson@users.noreply.github.com>
2018-01-29 15:59:55 +0100

    Initial Commit

---
+++ Labb3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Labb3
+//
+//  Created by will on 2018-01-29.
+//  Copyright © 2018 will. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Labb3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Labb3
+//
+//  Created by will on 2018-01-29.
+//  Copyright © 2018 will. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
